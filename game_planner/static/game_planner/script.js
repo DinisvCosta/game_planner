f54ff0ecb92574e18198ecea93681e632461f9ea
Dinis Costa <dinisvcosta@gmail.com>
2019-07-02 22:10:26 +0100

    Clean up friend requests handling

--- game_planner/static/game_planner/script.js
+++ game_planner/static/game_planner/script.js
@@ -24,24 +24,17 @@ function mark_as_read(notification_id) {
     xhttp.send(JSON.stringify(message));
 }
 
-function confirm_friend_request(pk) {
-
+function reply(func, state, pk, elem) {
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             // Remove friend_request from page
-            var name_element_id = "request_" + pk
-            var confirm_button_id = "request_" + pk + "_confirm_button"
-            var delete_button_id = "request_" + pk + "_delete_button"
-
-            document.getElementById(name_element_id).outerHTML = "";
-            document.getElementById(confirm_button_id).outerHTML = "";
-            document.getElementById(delete_button_id).outerHTML = "";
+            elem.parentNode.parentNode.removeChild(elem.parentNode);
         }
     };
 
-    xhttp.open("POST", "/friend_requests/", true);
+    xhttp.open("POST", func, true);
 
     //Send the proper header information along with the request
     xhttp.setRequestHeader("Content-Type", "application/json");
@@ -50,39 +43,7 @@ function confirm_friend_request(pk) {
     message = 
     {
         friend_request: pk,
-        state: "accepted"
-    }
-
-    xhttp.send(JSON.stringify(message));
-}
-
-function delete_friend_request(pk) {
-
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // Remove friend_request from page
-            var name_element_id = "request_" + pk
-            var confirm_button_id = "request_" + pk + "_confirm_button"
-            var delete_button_id = "request_" + pk + "_delete_button"
-
-            document.getElementById(name_element_id).outerHTML = "";
-            document.getElementById(confirm_button_id).outerHTML = "";
-            document.getElementById(delete_button_id).outerHTML = "";
-        }
-    };
-
-    xhttp.open("POST", "/friend_requests/", true);
-
-    //Send the proper header information along with the request
-    xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.setRequestHeader("X-CSRFToken", csrftoken);
-
-    message = 
-    {
-        friend_request: pk,
-        state: "declined"
+        state: state
     }
 
     xhttp.send(JSON.stringify(message));
