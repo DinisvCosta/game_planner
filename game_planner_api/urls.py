9fa26e3759af737d8582a69a09ae6aa369f2f0e0
Dinis Costa <dinisvcosta@gmail.com>
2019-10-29 17:13:32 +0000

    remove trailing slashes from api urls

--- game_planner_api/urls.py
+++ game_planner_api/urls.py
@@ -3,16 +3,16 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from game_planner_api import views
 
 urlpatterns = [
-    path('players/', views.PlayerList.as_view()),
-    path('players/<str:username>/', views.PlayerDetail.as_view(), name='player-detail'),
+    path('players', views.PlayerList.as_view()),
+    path('players/<str:username>', views.PlayerDetail.as_view(), name='player-detail'),
 
-    path('games/', views.GameList.as_view()),
-    path('games/<str:game_id>/', views.GameDetail.as_view(), name='game-detail'),
+    path('games', views.GameList.as_view()),
+    path('games/<str:game_id>', views.GameDetail.as_view(), name='game-detail'),
 
-    path('notifications/', views.NotificationList.as_view()),
+    path('notifications', views.NotificationList.as_view()),
     path('notifications/<int:id>', views.NotificationUpdate.as_view()),
 
-    path('friend_requests/', views.FriendRequestList.as_view()),
+    path('friend_requests', views.FriendRequestList.as_view()),
     path('friend_requests/<int:id>', views.FriendRequestDetail.as_view()),
 
 ]
